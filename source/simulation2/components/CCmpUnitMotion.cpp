ecec332a1c868d00f5484ecdcb36e74194b2cdb7
Lancelot de Ferrière <wraitii@gmail.com>
2017-01-13 16:40:30 +0100

    fix bug

--- source/simulation2/components/CCmpUnitMotion.cpp
+++ source/simulation2/components/CCmpUnitMotion.cpp
@@ -934,7 +934,7 @@ void CCmpUnitMotion::Move(fixed dt)
 	//////////////////////////////////////////////////////////////////////////
 
 	// we've had to stop at the end of the turn.
-	if (wasObstructed && m_StartedMoving)
+	if (m_StartedMoving)
 	{
 		StopMoving();
 
